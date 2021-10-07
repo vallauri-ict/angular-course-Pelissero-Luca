d43d2b137318bde767fbbbd3da81c691c84b3a21
Luca <l.pelissero.1360@vallauri.edu>
2021-10-07 10:46:42 +0200

    Lesson 1 - Step 3: ngStyle applied on student.component.html

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  // creazione lista studenti
+  studentList = [
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
+    { name: 'Luca Pelissero', hobby: 'Arrampicata', gender: 'M' },
+    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F' },
+    { name: 'Paolo Giordano', hobby: 'Volley', gender: 'M' },
+    { name: 'Michele Ghisolfi', hobby: 'Judo', gender: 'M' },
+    { name: 'Veronico Galvagno', hobby: 'Paddle', gender: 'F' },
+    { name: 'Chiara Pelissero', hobby: 'Paddle', gender: 'F' },
+    { name: 'Federico Mameli', hobby: 'Danza', gender: 'M' },
+    { name: 'Lorenzo Cravero', hobby: 'Calcio', gender: 'M' },
+    { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F' },
+    { name: 'Edoardo Ababei', hobby: 'Palestra', gender: 'M' },
+    { name: 'Ivan Anjelovski', hobby: 'Palestra', gender: 'M' }
+  ]
+
+  student : any;
+
+  constructor() {
+    // tutte le volte che viene istanziato uno studente ne viene preso uno a caso nella lista
+    let num = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[num];
+  }
 
   ngOnInit(): void {
   }
